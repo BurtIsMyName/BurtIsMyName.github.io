d884f80aa98b0ee85f2fe257825a2d0aa064b4cc
BurtIsMyName <bartolomeo.milossi@gmail.com>
2021-03-12 23:21:19 +0100

    json log

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ var card
 
 window.onload = () => {
     card = document.getElementsByClassName('card')
-    json = $.getJSON('123.json')
-    console.log(json);
+    json = $.getJSON('123.json', (json) => {
+        console.log(json);
+    })
 }
 
 function link(link) {
