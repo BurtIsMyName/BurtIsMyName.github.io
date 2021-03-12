85f88d7a91e4baa2881b8c4c45ed1fdc6414569e
BurtIsMyName <bartolomeo.milossi@gmail.com>
2021-03-12 23:27:12 +0100

    project json

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-var card
+var cardsGrid
 
 window.onload = () => {
-    card = document.getElementsByClassName('card')
+    cardsGrid = document.getElementById('cards-grid')
     json = $.getJSON('123.json', (json) => {
-        console.log(json);
+        cardsGrid.innerHTML += 
+        `<div class="card" onclick="link('/calculator/index.html')">
+            <div class="image-container">
+                <img src="${json.project[0].img}">
+            </div>
+            <strong href="${json.project[0].link}">${json.project[0].name}</strong>
+        </div>`
     })
 }
 
